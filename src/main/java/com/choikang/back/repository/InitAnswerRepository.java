cd899d14dd23d3bb2ebd68c1f9de90d955f0b621
Jyebin <yebin0322@naver.com>
2024-11-20 19:13:01 +0900

    feat : Add InitAnswerRepository

---
+++ src/main/java/com/choikang/back/repository/InitAnswerRepository.java
@@ -0,0 +1,8 @@
+package com.choikang.back.repository;
+
+import com.choikang.back.entity.InitAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InitAnswerRepository extends JpaRepository<InitAnswer, Integer> {
+
+}
